5d75e8afb397b1f2448df094287ed7cb796ba063
jaehwannnnnn <130552434+jaehwannnnnn@users.noreply.github.com>
2024-04-16 20:01:53 +0900

    [level 1] Title: 폰켓몬, Time: 5.41 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int[] arr = Arrays.stream(nums).distinct().toArray();
+        if(arr.length >= nums.length / 2){
+            answer = nums.length/2;
+        }else{
+            answer = arr.length;
+        }
+        return answer;
+    }
+}
