d9b3121cca16ffe018c633fa9c490b4c19cebcf5
jaehwannnnnn <130552434+jaehwannnnnn@users.noreply.github.com>
2025-02-26 15:00:59 +0900

    [난이도 1] Title: 운동 중독 플레이어, Time: 0.06 ms, Memory: 31.12 MB -BaekjoonHub

---
+++ goormlevel/195683/1. 운동 중독 플레이어/운동 중독 플레이어.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String[] strarr = input.split(" ");
+		double answer = Integer.parseInt(strarr[0]) * (double) (1 + (double)(Integer.parseInt(strarr[1]) / 30.0));
+		System.out.println((int)answer);
+	}
+}
