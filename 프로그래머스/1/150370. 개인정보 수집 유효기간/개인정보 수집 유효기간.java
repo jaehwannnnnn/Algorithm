a55ef92008c483ed29f6e47ab49b7e28e34deeab
jaehwannnnnn <130552434+jaehwannnnnn@users.noreply.github.com>
2025-02-28 15:27:34 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 31.03 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,28 +3,24 @@ import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        
         int todayYear = Integer.parseInt(today.substring(0, 4));
         int todayMonth = Integer.parseInt(today.substring(5, 7));
         int todayDay = Integer.parseInt(today.substring(8, 10));
-        
         String[][] sarr = new String[terms.length][2];
         for(int i = 0; i < sarr.length; i++){
             sarr[i] = terms[i].split(" ");
         }
-        
         for(int i = 0; i < privacies.length; i++){
             String year = privacies[i].substring(0, 4);
             String month = privacies[i].substring(5, 7);
             String day = privacies[i].substring(8, 10);
-            
             for(int j = 0; j < terms.length; j++){
                 if(privacies[i].charAt(privacies[i].length() - 1) == terms[j].charAt(0)){
                     int plus = Integer.parseInt(sarr[j][1]);
                     int newMonth = Integer.parseInt(month) + plus;
                     int yearPlus = newMonth / 12;
                     newMonth %= 12;
-                    if(newMonth == 0) {
+                    if(newMonth == 0){
                         newMonth = 12;
                         yearPlus--;
                     }
@@ -33,14 +29,18 @@ class Solution {
                     day = String.format("%02d", Integer.parseInt(day) - 1);
                 }
             }
-            
-            if(todayYear > Integer.parseInt(year) ||
-               (todayYear == Integer.parseInt(year) && todayMonth > Integer.parseInt(month)) ||
-               (todayYear == Integer.parseInt(year) && todayMonth == Integer.parseInt(month) && todayDay > Integer.parseInt(day))) {
-                answer.add(i + 1);
+            if(todayYear > Integer.parseInt(year)){
+                answer.add(i+1);
+            }else if(todayYear == Integer.parseInt(year)){
+                if(todayMonth > Integer.parseInt(month)){
+                    answer.add(i+1);
+                }else if(todayMonth == Integer.parseInt(month)){
+                    if(todayDay > Integer.parseInt(day)){
+                        answer.add(i+1);
+                    }
+                }
             }
         }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(x->x).toArray();
     }
 }
